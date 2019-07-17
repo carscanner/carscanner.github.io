b5e3f38c50eaf1ea73db3e91c87ea9fa2b5cbe38
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2019-07-17 21:37:00 +0200

    Updates

---
+++ precache-manifest.622a1047aee01f6f3f42cb33408f2f83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8389f21775b25c25a8591f465c86642",
+    "url": "./index.html"
+  },
+  {
+    "revision": "40ee4992fdc8dfed6966",
+    "url": "./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "813256dc454ec33e3034",
+    "url": "./static/css/main.3f965d86.chunk.css"
+  },
+  {
+    "revision": "40ee4992fdc8dfed6966",
+    "url": "./static/js/2.d1e1682e.chunk.js"
+  },
+  {
+    "revision": "813256dc454ec33e3034",
+    "url": "./static/js/main.09e43984.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
